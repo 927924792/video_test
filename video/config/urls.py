aa135b081be34954c99e805db9fd55c81fe9b31a
927924792 <927924792@qq.com>
2020-04-18 02:44:58 +0800

    第二次提交

--- video/config/urls.py
+++ video/config/urls.py
@@ -6,6 +6,7 @@ from app.client import urls as client_urls
 from app.dashboard import urls as dashboard_urls
 
 urlpatterns = [
-    path('client', include(client_urls)),
-    path('dashboard', include(dashboard_urls))
+    path('dashboard/', include(dashboard_urls)),
+    path('client/', include(client_urls)),
+
 ]
